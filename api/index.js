99c993156e91fa98f20069606e6979412bd81003
bem <bemnetadmasu9@gmail.com>
2026-05-20 01:38:03 +0300

    Fix server info detection for Vercel

--- api/index.js
+++ api/index.js
@@ -18,5 +18,12 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
+app.get('/api/server-info', (req, res) => {
+    res.json({
+        success: true,
+        ip: req.headers.host,
+        port: 443,
+        baseUrl: `https://${req.headers.host}`
+    });
+});
 module.exports = app;
